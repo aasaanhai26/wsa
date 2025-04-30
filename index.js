d5742a233f8e946f9f45b05f8c424cb1a30dfc5f
aasaanhai26 <aasaanhai2627@gmail.com>
2025-04-30 11:55:03 +0530

    salman

--- index.js
+++ index.js
@@ -11,12 +11,9 @@ dotenv.config();
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('MongoDB connected ✅'))
-.catch((err) => console.log('MongoDB connection error ❌:', err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected ✅'))
+  .catch((err) => console.log('MongoDB connection error ❌:', err));
 
 // Set view engine
 app.set('view engine', 'ejs');
